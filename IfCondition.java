ee2f1f03237b68407b173c0d3a788952500c3b2c
PavithraRanasinghe <paviranasinghe20@gamil.com>
2019-03-03 14:03:51 +0530

    first if code

---
+++ IfCondition.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class IfCondition{
+    public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input an integer : ");
+		int num=input.nextInt();
+		System.out.println("A");
+		System.out.println("B");
+		if(num>=50){
+			System.out.println("C");
+			System.out.println("D");
+		}
+		System.out.println("E");
+    }
+}
